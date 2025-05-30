5ac2289f9cd0419e44ff8f60f148ce471e589a04
momentchan <mingjyunhung@gmail.com>
2025-05-30 23:37:30 +0900

    add toggle debug

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,31 +4,43 @@ import PostPipeline from '../component/PostPipeline'
 import Utilities from '../r3f-gist/utility/Utilities'
 import fragmentShader from '../shader/test/fragment.glsl'
 import { CustomShaderMaterial } from '../r3f-gist/shader/CustomShaderMaterial'
+import { useEffect, useState } from 'react'
 
 function TorusMesh() {
-  return (
-    <mesh>
-      <boxGeometry args={[1, 1, 2]} />
-      <CustomShaderMaterial fragmentShader={fragmentShader} />
-    </mesh>
-  )
+    return (
+        <mesh>
+            <boxGeometry args={[1, 1, 2]} />
+            <CustomShaderMaterial fragmentShader={fragmentShader} />
+        </mesh>
+    )
 }
 
 export default function App() {
-  return (
-    <Canvas
-      shadows
-      camera={{ fov: 45, near: 0.1, far: 10, position: [4, 2, 6] }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <color attach="background" args={['#000']} />
-      <CameraControls makeDefault />
+    const [showDebug, setShowDebug] = useState(true)
 
-      <PostPipeline debug useEffectMaterial>
-        <TorusMesh />
-      </PostPipeline>
+    /* 監聽鍵盤 D → toggle */
+    useEffect(() => {
+        const handler = (e) => {
+            if (e.key.toLowerCase() === 'd') setShowDebug((v) => !v)
+        }
+        window.addEventListener('keydown', handler)
+        return () => window.removeEventListener('keydown', handler)
+    }, [])
 
-      <Utilities />
-    </Canvas>
-  )
+    return (
+        <Canvas
+            shadows
+            camera={{ fov: 45, near: 0.1, far: 10, position: [4, 2, 6] }}
+            gl={{ preserveDrawingBuffer: true }}
+        >
+            <color attach="background" args={['#000']} />
+            <CameraControls makeDefault />
+
+            <PostPipeline debug={showDebug} useEffectMaterial>
+                <TorusMesh />
+            </PostPipeline>
+
+            <Utilities />
+        </Canvas>
+    )
 }
